7d3cf4960f3e2ab424bc6fb697a426153355dafd
Emmanuil Galihanov <emikgal2507@gmail.com>
2025-02-20 20:41:05 +0500

    register add

---
+++ app/src/main/java/com/example/surchat/CommunicateBegin.kt
@@ -0,0 +1,27 @@
+package com.example.surchat
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CommunicateBegin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val openButton: Button = findViewById<Button>(R.id.communicate_begin)
+        openButton.setOnClickListener {
+            val intent = Intent(this, Authorization::class.java)
+            startActivity(intent)
+        }
+    }
+}
